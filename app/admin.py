3251170a742a1c2232bc3ea47f4332b14ca1625f
Hasan Mahmud <hmahmud01@gmail.com>
2022-04-09 02:52:06 +0600

    bonus added

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register([DashboardUser, UserRank, Service, ServiceType, Services, ServicePayments, Notices])
+admin.site.register([DashboardUser, UserRank, Service, ServiceType, Services, ServicePayments, Notices, SalesBonus])
